5709dfb236611ca5ed8261e1a3023fd1a3976161
Sergey Arnautov <sirius9312@ukr.net>
2023-12-01 12:46:22 +0200

    Updates

---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const populations = document.querySelectorAll(".population");
+const totalPopulation = document.querySelector(".total-population");
+const averagePopulation = document.querySelector(".average-population");
+const arrPopulations = [
+    ...populations
+].map((population)=>+population.textContent.split(",").join(""));
+const totalCountPeople = arrPopulations.reduce((prevPeople, people)=>prevPeople + people, 0);
+const averageCountPeople = Math.ceil(arrPopulations.reduce((prevPeople, people)=>prevPeople + people, 0) / arrPopulations.length);
+const numberFormatter = Intl.NumberFormat("en-US");
+const formattedTotalCountPeople = numberFormatter.format(totalCountPeople);
+const formattedAverageCountPeople = numberFormatter.format(averageCountPeople);
+totalPopulation.textContent = formattedTotalCountPeople;
+averagePopulation.textContent = formattedAverageCountPeople;
+
+//# sourceMappingURL=index.f75de5e1.js.map
